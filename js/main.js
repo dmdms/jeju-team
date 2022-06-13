4be7a55a5cfca638542e18a5c16e4f8168eb832e
e2j <e2j0017@gmail.com>
2022-06-13 12:18:28 +0900

    0613헤더푸터

---
+++ js/main.js
@@ -0,0 +1,19 @@
+$(function(){
+    scrollmenu();
+})
+
+function scrollmenu(){
+    $(window).scroll(function(){
+        if($(window).scrollTop()){
+            $('header').addClass('active');
+            $("#gnb").css('color','#333333');
+            $(".member").css('color','#333333');
+            $(".logo").removeClass("d-none");
+        } else{
+            $('header').removeClass('active');
+            $("#gnb").css('color','white');
+            $(".member").css('color','white');
+            $(".logo:last").addClass("d-none");
+        }
+    })
+}
