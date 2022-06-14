11cae5187386f805fc1d82a931e077aa061b84e0
unknown <eunji9688@naver.com>
2022-06-14 09:29:57 +0900

    images 수정

---
+++ script/main.js
@@ -0,0 +1,40 @@
+
+$(function(){
+    scrollmenu();
+})
+
+function scrollmenu(){
+    $(window).scroll(function(){
+        if($(window).scrollTop()){
+            $('header').addClass('active');
+            $("#gnb").css('color','#333333');
+            $(".member").css('color','#333333');
+            $(".logo").removeClass("d-none");
+        } else{
+            $('header').removeClass('active');
+            $("#gnb").css('color','white');
+            $(".member").css('color','white');
+            $(".logo:last").addClass("d-none");
+        }
+    })
+}
+
+
+var mapContainer = document.getElementById('map'), // 지도를 표시할 div 
+    mapOption = { 
+        center: new kakao.maps.LatLng(33.450701, 126.570667), // 지도의 중심좌표
+        level: 3 // 지도의 확대 레벨
+    };
+
+var map = new kakao.maps.Map(mapContainer, mapOption); // 지도를 생성합니다
+
+// 마커가 표시될 위치입니다 
+var markerPosition  = new kakao.maps.LatLng(33.450701, 126.570667); 
+
+// 마커를 생성합니다
+var marker = new kakao.maps.Marker({
+    position: markerPosition
+});
+
+// 마커가 지도 위에 표시되도록 설정합니다
+marker.setMap(map);
