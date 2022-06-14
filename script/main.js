56d076714110ef4bf07888b141c2fe65e7fc81ba
your-name-here <100328759+heejichoi@users.noreply.github.com>
2022-06-14 12:33:37 +0900

    basket complete

---
+++ script/main.js
@@ -0,0 +1,28 @@
+$(function(){
+    scrollmenu();
+    popup();
+})
+
+function scrollmenu(){
+    $(window).scroll(function(){
+        if($(window).scrollTop()){
+            $('header').addClass('active');
+            $("#gnb").css('color','#333333');
+            $(".member").css('color','#333333');
+            $(".logo").removeClass("d-none");
+        } else{
+            $('header').removeClass('active');
+            $("#gnb").css('color','white');
+            $(".member").css('color','white');
+            $(".logo:last").addClass("d-none");
+        }
+    })
+}
+function popup(){
+    $(".cart-icon").click(function(){
+        $("#modal").show();
+    });
+    $("#modal button").click(function(){
+        $("#modal").hide();
+    });
+}
