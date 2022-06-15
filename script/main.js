ba9eabd0d08ba470f2e8b7df85c8b8c26dd773dd
unknown <eunji9688@naver.com>
2022-06-15 16:54:55 +0900

    지도, 테마별 수정

--- script/main.js
+++ script/main.js
@@ -1,4 +1,3 @@
-
 $(function(){
     scrollmenu();
     tabMenu();
@@ -22,23 +21,44 @@ function scrollmenu(){
 
 function tabMenu(){
     $(".t-icon1").click(function(){
-        $(".theme-img1").show();
+        $(".theme-img1").fadeIn();
         $(".theme-img2").hide();
         $(".theme-img3").hide();
         $(".theme-img4").hide();
-        $(".theme-info1").show();
+        $(".theme-info1").fadeIn();
         $(".theme-info2").hide();
         $(".theme-info3").hide();
         $(".theme-info4").hide();
     });
     $(".t-icon2").click(function(){
         $(".theme-img1").hide();
-        $(".theme-img2").show();
+        $(".theme-img2").fadeIn();
         $(".theme-img3").hide();
         $(".theme-img4").hide();
         $(".theme-info1").hide();
-        $(".theme-info2").show();
+        $(".theme-info2").fadeIn();
         $(".theme-info3").hide();
         $(".theme-info4").hide();
     })
+    $(".t-icon3").click(function(){
+        $(".theme-img1").hide();
+        $(".theme-img2").hide();
+        $(".theme-img3").fadeIn();
+        $(".theme-img4").hide();
+        $(".theme-info1").hide();
+        $(".theme-info2").hide();
+        $(".theme-info3").fadeIn();
+        $(".theme-info4").hide();
+    });
+    $(".t-icon4").click(function(){
+        $(".theme-img1").hide();
+        $(".theme-img2").hide();
+        $(".theme-img3").hide();
+        $(".theme-img4").fadeIn();
+        $(".theme-info1").hide();
+        $(".theme-info2").hide();
+        $(".theme-info3").hide();
+        $(".theme-info4").fadeIn();
+    })
+
 }
