3c93e0f7221f3ea175ad6409376ceb0b9febcdae
unknown <eunji9688@naver.com>
2022-06-16 19:35:52 +0900

    guide 페이지 반응형 작성

---
+++ script/guide.js
@@ -0,0 +1,36 @@
+let curPos = 0;
+let postion = 0;
+let start_x, end_x;
+const IMAGE_WIDTH = 375;
+const images = document.querySelector(".mg-images") 
+ 
+images.addEventListener('touchstart', touch_start);
+images.addEventListener('touchend', touch_end);
+ 
+function prev(){
+  if(curPos > 0){
+    postion += IMAGE_WIDTH;
+    images.style.transform = `translateX(${postion}px)`;
+    curPos = curPos - 1;
+  }
+}
+function next(){
+  if(curPos < 2){
+    postion -= IMAGE_WIDTH;
+    images.style.transform = `translateX(${postion}px)`;
+    curPos = curPos + 1;
+  }
+}
+ 
+function touch_start(event) {
+  start_x = event.touches[0].pageX
+}
+ 
+function touch_end(event) {
+  end_x = event.changedTouches[0].pageX;
+  if(start_x > end_x){
+    next();
+  }else{
+    prev();
+  }
+}
